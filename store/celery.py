f149fe7170ded05236cfb2a0a62ce953e8ad2e49
Ma-Yuhao <73241641+Ma-Yuhao@users.noreply.github.com>
2021-04-08 01:22:27 +0800

    Delete store directory

--- store/celery.py
+++
@@ -1,7 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
-app = Celery('store', broker='redis://127.0.0.1:6379/6')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
